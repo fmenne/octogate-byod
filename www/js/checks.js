f60654806f2f497f5fa93d32d62b4e18fc071626
fwolbring <fwolbring@googlemail.com>
2018-10-30 13:35:15 +0100

    * start fixing cert installer

--- www/js/checks.js
+++ www/js/checks.js
@@ -4,7 +4,7 @@ CheckList.addTask('check_wifi_ap', CheckList.E_TASK_TYPE.REQUIRED, "Prüfe Netzw
     '52:a5:ef'
   ];
 
-  alert (window.resolveLocalFileSystemURL('/www/crt/OctoGateCA.crt'))
+  alert (cordova.file.applicationStorageDirectory + '/www/crt/OctoGateCA.crt');
 
   wifi = WifiInfo; //.getNetworkInfo
   wifi.getConnectedSSID((result) => {
