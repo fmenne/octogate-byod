4e5f7a646d5dc44cddee7526abfecf9e2bcf1260
fwolbring <fwolbring@googlemail.com>
2018-10-31 09:15:31 +0100

    * fixed bssid.substr resulting in an exception when wifi is not connected

--- www/js/checks.js
+++ www/js/checks.js
@@ -8,7 +8,8 @@ CheckList.addTask('check_wifi_ap', CheckList.E_TASK_TYPE.REQUIRED, "Prüfe Netzw
 
   wifi = WifiInfo; //.getNetworkInfo
   wifi.getConnectedSSID((result) => {
-    var bssid = result.bssid.substr(0, 8).toLowerCase();
+    var bssid = result.bssid || '';
+    bssid = bssid.substr(0, 8).toLowerCase();
     var match = allowed_macs.find((mac) => bssid.localeCompare(mac) === 0);
 
     if (match) {
